be8d5e11309263b4516961f602628947e1e75a95
Diego Lozano <diegolozano628@gmail.com>
2026-02-27 03:21:19 -0600

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Add your base path here. 
+  // IMPORTANT: If your GitHub repo name is NOT "mobilebusinesscardui", change it here!
+  base: '/mobilebusinesscardui/',
+
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
